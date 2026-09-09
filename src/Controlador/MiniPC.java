92dfff79a7600e0cfc64592af9d148eb867417e7
Diego <daraya0906@gmail.com>
2026-09-08 20:52:11 -0600

    Inicializacion de Interfaz grafica y separacio nde modelo vista controlador

---
+++ src/Controlador/MiniPC.java
@@ -0,0 +1,12 @@
+package Controlador;
+
+public class MiniPC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
